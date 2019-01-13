af05ae23f0b576243c3c1cb5bae06420cf791691
João P. Esperidião <Joao@MacPreto.local>
2019-01-13 03:26:50 -0200

    code's structure defined.

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ MOPA
+                                 An independet project
+ Método de Obtenção da Posição de Atirador
+                              -------------------
+        begin                : 2018-12-21
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by João P. Esperidião
+        email                : joao.p2709@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
